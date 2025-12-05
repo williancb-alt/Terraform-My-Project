7a6dcc5e57b577c0b14ec4127018ebf98a6793f8
L00203060 <L00203060@atu.ie>
2025-12-05 12:28:24 +0000

    Enhance variable definitions with comments
    Added comments to clarify variable purpose and usage.

--- modules/alb/variables.tf
+++ modules/alb/variables.tf
@@ -1,3 +1,9 @@
+# Defines three input variables to parameterize the configuration, enabling reuse across environments or modules.
+#Accepts a string value for the AWS VPC ID where resources will be created
 variable "vpc_id" {}
+
+#Expects a list of strings containing public subnet IDs
 variable "public_subnets" { type = list(string) }
+
+# Takes a string value for an existing EC2 security group ID, typically passed from another module's output
 variable "ec2_sg_id" {}
