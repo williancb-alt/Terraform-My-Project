9f73410689d21967ad6fc4b742491299108836ea
L00203060 <L00203060@atu.ie>
2025-12-05 15:47:20 +0000

    Removing duplications
    Refactor AWS provider and module declarations for clarity.

--- main.tf
+++ main.tf
@@ -1,36 +1,3 @@
-# Declares the required AWS provider from HashiCorp's registry and constrains it to version ~> 5.0
-terraform {
-  required_providers {
-    aws = { source = "hashicorp/aws", version = "~> 5.0" }
-  }
-}
-
-# Configures to deploy all resources (VPC, EC2, ALB, etc.) in the region specified by var.aws_region
-provider "aws" { region = var.aws_region }
-
-# Calls local VPC module from ./modules/vpc/, passing availability_zones variable to create a VPC infrastructure such as 
-# VPC, public/private subnets, NAT, route tables, IGW, etc.
-module "vpc" { 
-  source = "./modules/vpc" 
-  availability_zones = var.availability_zones
-}
-module "security" {
-  source = "./modules/security"
-  vpc_id = module.vpc.vpc_id 
-}
-module "alb" { 
-  source = "./modules/alb"
-  vpc_id = module.vpc.vpc_id
-  public_subnets = module.vpc.public_subnets
-  ec2_sg_id = module.security.ec2_sg_id
-}
-module "ec2" { 
-  source = "./modules/ec2"
-  private_subnets = module.vpc.private_subnets
-  ec2_sg_id = module.security.ec2_sg_id
-  target_group_arn = module.alb.target_group_arn
-}
-
 # Declares provider requirements for the configuration.
 # Sets up the AWS provider and orchestrates four modules to create a complete AWS web infrastructure stack.
 terraform {
@@ -39,7 +6,9 @@ terraform {
   }
 }
 
-provider "aws" { region = var.aws_region }
+provider "aws" {
+  region = var.aws_region 
+}
 
 # Creates VPC infrastructure and outputs vpc_id, public_subnets, and private_subnets.
 module "vpc" { 
