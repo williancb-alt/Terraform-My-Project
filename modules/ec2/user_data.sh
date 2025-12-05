607a3bbeab5961706e643f0a258802ce1d10864b
L00203060 <L00203060@atu.ie>
2025-12-05 11:47:20 +0000

    Enhance user_data.sh with descriptive comments
    Added comments to explain the purpose of the script.

--- modules/ec2/user_data.sh
+++ modules/ec2/user_data.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# This script bootstraps an EC2 instance by updating the OS, installing and starting the Apache web server,
+# enabling it to run on startup, and creating a simple homepage that displays the instance's hostname 
+# and availability zone. This demonstrate multi-AZ deployment by showing which AZ serves the webpage.
 yum update -y
 yum install -y httpd
 systemctl start httpd
